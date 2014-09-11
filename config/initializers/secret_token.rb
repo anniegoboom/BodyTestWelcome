9da433187d26d74a728fc0f2cb4f27fb5a554f06
Annie Zhou <annie@lumoslabs.com>
2014-09-10 21:52:26 -0700

    starting the new welcome funnel app!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BodyTestWelcome::Application.config.secret_token = 'eef99c15053434cb07a9bab471a49ffcb22745c7f5390bf5187f64bdddffc9ce5256634392323c2e1449fd3a7c075d92a1578fe7d4bbd36d16e56f081414a18f'
