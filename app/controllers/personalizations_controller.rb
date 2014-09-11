4f9cfa150b3bce93319cad9f5b04bdc9b558c7c0
Annie Zhou <annie@lumoslabs.com>
2014-09-11 02:04:32 -0700

    added a wizard in personalization. have to fix devise to authenticate a user with only an email before i can start using it

--- app/controllers/personalizations_controller.rb
+++ app/controllers/personalizations_controller.rb
@@ -1,4 +1,16 @@
+#https://github.com/plataformatec/devise/wiki/How-To:-Email-only-sign-up
+
 class PersonalizationsController < ApplicationController
+  # include Wicked::Wizard
+
+  # steps :user_information, :goals, :starting_point
+
+  # def show
+  #   @user = current_user
+  #   binding.pry
+  #   render_wizard
+  # end
+
   def show
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates_presence_of :email
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   # attr_accessible :title, :body
