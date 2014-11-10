ce961ab9ce9712cf9c1ab2feee80f93515190cf5
Annie <annie@lumoslabs.com>
2014-11-10 01:35:15 -0800

    adding heroku as config host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,9 @@ BodyTestWelcome::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  # heroku host
+  config.action_mailer.default_url_options = { :host => 'body-test-welcome.herokuapp.com' }
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
