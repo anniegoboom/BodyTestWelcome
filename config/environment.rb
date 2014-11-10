25b5a45f3be2ce443e22556b7e4c04e97c85bbf7
Annie <annie@lumoslabs.com>
2014-11-10 02:30:58 -0800

    adding smtp to enviornment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,21 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 BodyTestWelcome::Application.initialize!
+
+# Mailer for devise
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.raise_delivery_errors = false
+ActionMailer::Base.default :charset => "utf-8"
+
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.default_url_options = { :host => 'http://body-test-welcome.herokuapp.com' }
+
+ActionMailer::Base.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: ENV["GMAIL_DOMAIN"],
+  authentication: :plain,
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+}
