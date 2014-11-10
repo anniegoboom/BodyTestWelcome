1c0d9c806272da437d2ff5b06a7714b00c7d9ff1
Annie <annie@lumoslabs.com>
2014-11-10 00:56:43 -0800

    redirect_to_paywall

--- app/controllers/landing_flows_controller.rb
+++ app/controllers/landing_flows_controller.rb
@@ -1,4 +1,18 @@
 class LandingFlowsController < ApplicationController
+  before_filter :redirect_if_logged_in, only: :show
+
   def show
+  end
+
+  def redirect_if_logged_in
+    redirect_to_paywall if logged_in?
+  end
+
+  def redirect_to_paywall
+    redirect_to PersonalizationSurveysController::BODY_TEST_PAYMENTS_URL
+  end
+
+  def logged_in?
+    !current_user.nil?
   end
 end
